9082eea23d9a319cd93f5755cdd0fecdaf2141c9
Himanshu Thakur <61612477+himanshu-thakur-7@users.noreply.github.com>
2024-04-20 22:11:39 +0530

    Added GCP Storage setup

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,32 @@
 const express = require("express");
+const { Storage } = require('@google-cloud/storage');
+
 const PORT = 8000 | process.env.PORT;
 
 const app = express();
 
 app.use(express.json());
 
-app.get("/", (res, req) => {
-    res.json("Hello world")
+app.get("/", (req, res) => {
+
+    // Creates a client
+    const storage = new Storage({
+        keyFilename: "./keys/gcp_key.json"
+    });
+
+    async function listFiles() {
+        const bucketName = "ide7781"
+        // Lists files in the bucket
+        const [files] = await storage.bucket(bucketName).getFiles();
+
+        console.log('Files:');
+        files.forEach(file => {
+            console.log(file.name);
+        });
+    }
+
+    listFiles().catch(console.error);
+    res.json({ "res": "Hello" })
 })
 
 app.listen(PORT, () => {
