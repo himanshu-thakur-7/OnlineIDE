9bdea96a3a2bab56dc4677ffe6d311c847a96acf
Himanshu Thakur <61612477+himanshu-thakur-7@users.noreply.github.com>
2024-05-04 16:33:58 +0530

    added fetch directory and file content events

--- backend/test.js
+++
@@ -1,9 +0,0 @@
-const { fetchDir } = require("./fs");
-
-const main = async () => {
-    const files = await fetchDir("./tmp/a5a25aba-ae11-4743-a0ea-a1c13c573954", "")
-
-    console.log(files);
-
-}
-main();
--- backend/ws.js
+++ backend/ws.js
@@ -1,7 +1,7 @@
 const { Server, Socket } = require("socket.io");
 const { path } = require("path")
 const { saveFilesFromGCP } = require("./gcp");
-const { fetchDir } = require("./fs");
+const { fetchDir, fetchFileContent } = require("./fs");
 const initWs = (httpServer) => {
     console.log('hi line 6')
     const io = new Server(httpServer, {
@@ -29,10 +29,27 @@ const initWs = (httpServer) => {
             });
 
         })
+
+        helper(socket, replId);
     }
     catch (e) {
         console.log(e)
     }
+}
+
+const helper = (socket, replId) => {
+    socket.on("disconnect", () => {
+        console.log("Disconnected")
+    })
+
+    socket.on('fetchDir', async (dirname, cb) => {
+        const contents = await fetchDir(`./tmp/${replId}/${dirname}`, dirname);
+        cb(contents);
+    })
+    socket.on('fetchContent', async (filePath, cb) => {
+        const contents = await fetchFileContent(`./tmp/${replId}/${filePath}`);
+        cb(contents);
+    })
 }
 
 module.exports = { initWs }
