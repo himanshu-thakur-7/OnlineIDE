dc8d103ef003ba74c336b52aa8009e794ed99d77
Himanshu Thakur <61612477+himanshu-thakur-7@users.noreply.github.com>
2024-06-11 00:12:55 +0530

    Added rust icon

--- frontend/src/components/widgets/Tree/FileIcons.jsx
+++ frontend/src/components/widgets/Tree/FileIcons.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { DiJavascript1, DiCss3Full, DiHtml5, DiReact, DiPython, DiGo } from "react-icons/di";
+import { DiJavascript1, DiCss3Full, DiHtml5, DiReact, DiPython, DiGo, DiRust } from "react-icons/di";
 import { VscJson } from "react-icons/vsc";
 const FILE_ICONS = {
   js: <DiJavascript1 />,
@@ -8,7 +8,8 @@ const FILE_ICONS = {
   jsx: <DiReact />,
   py: <DiPython />,
   json: <VscJson />,
-  go: <DiGo style={{ color: "#FEF9C3" }} />
+  go: <DiGo style={{ color: "#FEF9C3" }} />,
+  rs: <DiRust style={{ width: 22, height: 22 }} />
 };
 
 export default FILE_ICONS;
